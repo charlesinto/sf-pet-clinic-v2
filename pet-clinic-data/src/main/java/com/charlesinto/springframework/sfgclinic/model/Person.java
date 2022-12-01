ab0da4448c49ca78c94d054db1c5bf281d6bcfa8
charles <charles.onuorah@yahoo.com>
2022-12-01 13:05:34 +0100

    Add ID to models. closes #10

--- pet-clinic-data/src/main/java/com/charlesinto/springframework/sfgclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/charlesinto/springframework/sfgclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.charlesinto.springframework.sfgclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private  String lastName;
--- pet-clinic-data/src/main/java/com/charlesinto/springframework/sfgclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/charlesinto/springframework/sfgclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.charlesinto.springframework.sfgclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
